91517b5e73c288fc9528d5d530b11646439dafe3
thomas_lorthioir <tottilorthioir@gmail.com>
2022-09-27 16:31:48 -0400

    feat: (app) fetch summoner by name

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+// To use APIKEY
+require('dotenv').config();
+const api_key = process.env.LOL_API_KEY;
 const axios = require('axios');
 const app = express();
-const api_key = process.env.REACT_APP_API_KEY;
 
 
 
@@ -10,9 +12,27 @@ app.get('/getAllChampInfos/:champName', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     const test = axios.get(`http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions/${req.params.champName}.json`)
         .then(function (response) {
-            // handle success
-            // console.log('MERAKIANA', response);
-            console.log(response.data);
+            res.send(response.data)
+
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+
+        });
+
+    // console.log(test)
+    // res.json([test])
+})
+
+app.get('/getSummoner/:summonerName', (req, res) => {
+
+    res.set('Access-Control-Allow-Origin', '*');
+    const test = axios.get(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.summonerName}?api_key=${api_key}`)
+        .then(function (response) {
             res.send(response.data)
 
         })
